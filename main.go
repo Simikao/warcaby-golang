b36cdb7f3d77239f2dfbeab2af19165f60b86092
Simikao <s.wierzejski.420@studms.ug.edu.pl>
2025-02-02 22:06:31 +0100

    feat: Getting input

--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
 	game "warcaby/warcaby"
 )
 
 func main() {
 	g := game.NewGame(1)
+	scanner := bufio.NewScanner(os.Stdin)
 
-	g.PrintBoard()
+	for {
+		g.PrintBoard()
+
+		fmt.Println("Podaj ruch w formacie: fromX fromY toX toY (lub wpisz 'exit' aby zakończyć):")
+		if !scanner.Scan() {
+			break
+		}
+
+		input := strings.TrimSpace(scanner.Text())
+		if input == "exit" {
+			break
+		}
+
+		parts := strings.Fields(input)
+		if len(parts) != 4 {
+			fmt.Println("Nieprawidłowy format. Spróbuj ponownie.")
+			continue
+		}
+
+		fmt.Println(parts)
+	}
 }
